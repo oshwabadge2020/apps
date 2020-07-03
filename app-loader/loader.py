a69ba00fa21e02916c56788b927a576cdc03b2a0
siddacious <nospam187+github@gmail.com>
2020-07-02 19:13:02 -0700

    updating loader

--- app-loader/loader.py
+++ app-loader/loader.py
@@ -8,6 +8,20 @@ from time import sleep
 import terminalio
 from adafruit_display_text import label
 import displayio
+
+# TODO:
+"""
+# default app to run; override by holding buttons
+# set screen and button rotation
+# auto dim/shut off screen by looking for significant motion via accel
+# battery gauge
+
+
+
+
+
+
+###
 SELECT = 1 << 0
 NEXT = 1 << 1
 PREV = 1 << 2
@@ -17,8 +31,11 @@ TOP_OFFSET = 3
 MARGIN = 3
 APP_DIR = "/apps"
 MENU_START = 10+TOP_OFFSET+MARGIN
-SPLASH_FILENAME = "10yrs_240.bmp"
-CURSOR_FILENAME = "8px_cursors.bmp"
+FRAME_FILENAME = "/apps/loader/frame2.bmp"
+SPLASH_FILENAME = "/apps/loader/10yrs_240.bmp"
+CURSOR_FILENAME = "/apps/loader/8px_cursors.bmp"
+# load rotation config; hardcoded for now
+board.DISPLAY.rotation = 90
 class Loader:
     def __init__(self):
         self.sw1 = None
@@ -27,7 +44,7 @@ class Loader:
         self.sw4 = None
 
         self.display = board.DISPLAY
-        self.display.brightness = 0.0
+        self.display.brightness = 1.0
         self.cursor_index = 0
         self.init_cursor()
 
@@ -59,11 +76,20 @@ class Loader:
         self.cursor.y = MENU_START+(self.cursor_index*8)
 
     def init_menu(self):
+        frame_bmp, frame_pal = adafruit_imageload.load(FRAME_FILENAME, bitmap=displayio.Bitmap, palette=displayio.Palette)
+        print("Frame palette", frame_pal[0])
+        print("Frame palette", frame_pal[1])
+        frame_pal.make_transparent(0)
+        frame_pal[1] = 0xffffff
+        frame_pal[0] = 0x0
+        frame_pal.make_transparent(1)
         self.program_menu = displayio.Group(max_size=10, scale=2)
         loader_banner = label.Label(FONT, text="Choose to Run", color=COLOR)
         loader_banner.x = 10
         loader_banner.y = TOP_OFFSET
         self.program_menu.append(loader_banner)
+        frame = displayio.TileGrid(frame_bmp, pixel_shader=frame_pal)
+
         for list_index, program_name in enumerate(self.files_available):
             menu_item_str = "%s"%'{:>5}'.format(program_name)
 
@@ -75,6 +101,7 @@ class Loader:
 
         self.program_menu.append(self.cursor)
 
+        self.program_menu.append(frame)
     def init_buttons(self):
         self.sw1 = digitalio.DigitalInOut(board.BUTTON_SW1)
         self.sw2 = digitalio.DigitalInOut(board.BUTTON_SW2)
@@ -103,50 +130,20 @@ class Loader:
                 break
 
     def check_for_apps(self, appdir=APP_DIR):
-        return os.listdir(appdir)
+        file_list = []
+        try:
+            file_list = os.listdir(appdir)
+        except:
+            os.mkdir(appdir)
 
-    #def print_list(display, programs, cur_index):
+        return file_list
 
-    if False:
-        pass
-        #
-        #           MICRO USB
-        #     BUTTON_SW3  BUTTON_SW4
-        #     _____________________
-        #     |                    |
-        #     |                    |
-        #     |                    |
-        #     |                    |
-        #     |                    |
-        #     |                    |
-        #     |____________________|
-        #     BUTTON_SW2, BUTTON_SW1
-        #
-
-
-    # Open the file
-
-    # splash_bmp, splash_pal = adafruit_imageload.load(SPLASH_FILENAME, bitmap=displayio.Bitmap, palette=displayio.Palette)
-    # Create a Group to hold the TileGrid
-    # splash_group = displayio.Group()
-    # board.DISPLAY.brightness = 0.0
-    # time.sleep(2)
-    # loader_banner = label.Label(FONT, text="Cursor Index: 0", color=COLOR)
-    # loader_banner.x = 10
-    # loader_banner.y = 160
-    # display.show(loader_banner)
-
-
-    # splash = displayio.TileGrid(splash_bmp, pixel_shader=splash_pal)
-    # splash_group.append(splash)
-    # splash_group.scale = 1
-    # display.show(splash_group)
     def run(self):
-        last_update =  time.monotonic()
+        last_update = time.monotonic()
         i = 0
         current_time = time.monotonic()
         while True:
-            current_time =  time.monotonic()
+            current_time = time.monotonic()
             if current_time - last_update > 0.2:
                 last_update = current_time
                 i +=1
@@ -169,6 +166,23 @@ class Loader:
                 # Wait for all buttons to be released.
                 buttons = self.pad.get_pressed()
 
+
 if __name__ == "__main__":
+
+    display = board.DISPLAY
+    # Open the file
+    splash_bmp, splash_pal = adafruit_imageload.load(SPLASH_FILENAME, bitmap=displayio.Bitmap, palette=displayio.Palette)
+
+    # Create a Group to hold the TileGrid
+    splash_group = displayio.Group()
+    splash = displayio.TileGrid(splash_bmp, pixel_shader=splash_pal)
+    splash_group.append(splash)
+    splash_group.scale = 1
+    display.show(splash_group)
+    sleep(0.5)
+    del(splash_bmp)
+    del(splash_pal)
+
+
     loader = Loader()
     loader.run()
---
+++ i2c_scanner/i2c_scanner.py
@@ -0,0 +1,47 @@
+import time
+import terminalio
+import displayio
+import board
+from adafruit_display_text import label
+
+Y_OFFSET=20
+X_INDENT=10
+def main():
+  display = board.DISPLAY
+  i2c = board.I2C()
+  
+  
+  display = board.DISPLAY
+  font = terminalio.FONT
+  t_group = displayio.Group()
+  while True:
+      while not i2c.try_lock():
+          time.sleep(0.010)
+      while len(t_group) > 0:
+          t_group.pop()
+      things = i2c.scan()
+  
+      if len(things) == 0:
+          print("No I2C Devices responded to scan")
+          # Create the test label
+          not_found = label.Label(terminalio.FONT, text="No I2C Devices \nresponded to scan", color=0xFFFFFF, scale=2)
+          not_found.x = X_INDENT
+          not_found.y = Y_OFFSET+80
+          t_group.append(not_found)
+  
+      else:
+          found_str = ""
+          for index, thing in enumerate(things):
+              found_str +="Found %d (%s)\n"%(thing, hex(thing))
+          found = label.Label(terminalio.FONT, text=found_str, color=0xFFFFFF, scale=2)
+          found.x = X_INDENT
+          found.y = Y_OFFSET+20
+          t_group.append(found)
+          print(found_str)
+      i2c.unlock()
+      display.show(t_group)
+  
+      print("*************** Done! *************")
+  
+      time.sleep(1)
+  
